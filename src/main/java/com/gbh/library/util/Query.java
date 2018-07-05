e385a9d70c76606bfbd48127a7f283f46caba2c6
yinfante@encorerepair.com <LEIRAYay000>
2018-07-05 19:16:21 -0400

    Getting page by page number and book

--- src/main/java/com/gbh/library/util/Query.java
+++ src/main/java/com/gbh/library/util/Query.java
@@ -6,6 +6,6 @@ public final class Query {
 	public final static String SELECT_ALL_BOOK_BY_ID = "SELECT ID, AUTHOR, TITLE, PAGES, COVER_URL FROM BOOK WHERE ID = ?";
 	
 	public final static String SELECT_PAGES_BY_BOOK_ID = "SELECT ID, NUMBER, CONTENT, BOOK_ID FROM PAGE WHERE BOOK_ID = ?";
-	public final static String SELECT_PAGES_BY_ID = "SELECT ID, NUMBER, CONTENT, BOOK_ID FROM PAGE WHERE ID = ? AND BOOK_ID = ?";
+	public final static String SELECT_PAGES_BY_ID = "SELECT ID, NUMBER, CONTENT, BOOK_ID FROM PAGE WHERE NUMBER = ? AND BOOK_ID = ?";
 
 }
